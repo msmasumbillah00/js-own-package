8cd7ceb0d83a52b993dd5956259d3e80005de0f9
Md Masum Billah <mdmasumbillahms00@gmail.com>
2023-05-07 13:15:20 +0800

    mofify store function

--- utilities.js
+++ utilities.js
@@ -1,37 +1,46 @@
+//------------------------ function for single chart product  only------------- 
 const  chartSingleDb=(id,quantity)=>{
     //  check if the key exiest in local-stroge
+
+    // if exiest the value will update only
      if(localStorage.getItem(id)){
         let quantity=JSON.parse(localStorage.getItem(id))+1;
         localStorage.setItem(id,quantity)
     }
+
+    // if not exiest the key and value will be added;
     else{
         localStorage.setItem(id,1)
     }
 }
 
-const  chartMultiDb=(id,value)=>{
+//------------------------- function for multi  chart products-------------------
+const  chartMultiDb=(key,id)=>{
 
-    if(localStorage.getItem(id)){
-       const oldData=JSON.parse(localStorage.getItem(id));
+    if(localStorage.getItem(key)){ //------chect first if already exiest the key in local stroge
+       const oldData=JSON.parse(localStorage.getItem(key)); 
        let chart=oldData;
-       if(chart[value]){
-        chart[value]=chart[value]+1;
+       if(chart[id]){
+        chart[id]=chart[id]+1;
         
        }
        else{
-        chart[value]=1;
+        chart[id]=1;
        }
        const newData=JSON.stringify(chart);
-        localStorage.setItem(id,newData)
+        localStorage.setItem(key,newData)
        
     }
     else{
         let chart={};
-        chart[value]=1;
+        chart[id]=1;
         const data=JSON.stringify(chart);
-        localStorage.setItem(id,data)
+        localStorage.setItem(key,data)
     }
 }
+
+
+
 const  addToSingleDb=(id)=>{
    
 }
@@ -40,9 +49,20 @@ const  addToMultiDb=(id,value)=>{
 
 }
 
+const getShoppingCart = (key) => {
+    let shoppingCart = {};
+
+    //get the shopping cart from local storage
+    const storedCart = localStorage.getItem(key);
+    if (storedCart) {
+        shoppingCart = JSON.parse(storedCart);
+    }
+    return shoppingCart;
+}
 
 export {
     chartSingleDb,
-    chartMultiDb
+    chartMultiDb,
+    getShoppingCart
 
 }
