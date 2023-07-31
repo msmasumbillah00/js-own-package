592f7bd9445137abbcfe14283b3b365178f8bffd
Md Masum Billah <mdmasumbillahms00@gmail.com>
2023-08-01 07:58:40 +0800

    update single data base

--- utilities.js
+++ utilities.js
@@ -1,68 +1,72 @@
 //------------------------ function for single chart product  only------------- 
-const  chartSingleDb=(id,quantity)=>{
+const chartSingleDb = (id, quantity) => {
     //  check if the key exiest in local-stroge
 
     // if exiest the value will update only
-     if(localStorage.getItem(id)){
-        let quantity=JSON.parse(localStorage.getItem(id))+1;
-        localStorage.setItem(id,quantity)
+    if (localStorage.getItem(id)) {
+        let quantity = JSON.parse(localStorage.getItem(id)) + 1;
+        localStorage.setItem(id, quantity)
     }
 
     // if not exiest the key and value will be added;
-    else{
-        localStorage.setItem(id,1)
+    else {
+        localStorage.setItem(id, 1)
     }
 }
 
 //------------------------- function for multi  chart products-------------------
-const  chartMultiDb=(key,id)=>{
+const chartMultiDb = (key, id) => {
 
-    if(localStorage.getItem(key)){ //------chect first if already exiest the key in local stroge
-       const oldData=JSON.parse(localStorage.getItem(key)); 
-       let chart=oldData;
-       if(chart[id]){
-        chart[id]=chart[id]+1;
-        
-       }
-       else{
-        chart[id]=1;
-       }
-       const newData=JSON.stringify(chart);
-        localStorage.setItem(key,newData)
-       
+    if (localStorage.getItem(key)) { //------chect first if already exiest the key in local stroge
+        const oldData = JSON.parse(localStorage.getItem(key));
+        let chart = oldData;
+        if (chart[id]) {
+            chart[id] = chart[id] + 1;
+
+        }
+        else {
+            chart[id] = 1;
+        }
+        const newData = JSON.stringify(chart);
+        localStorage.setItem(key, newData)
+
     }
-    else{
-        let chart={};
-        chart[id]=1;
-        const data=JSON.stringify(chart);
-        localStorage.setItem(key,data)
+    else {
+        let chart = {};
+        chart[id] = 1;
+        const data = JSON.stringify(chart);
+        localStorage.setItem(key, data)
     }
 }
 
 
 
-const  addToSingleDb=(id)=>{
-   
+const addToSingleDb = (id, value) => {
+    let data = {};
+    data[id] = value;
+    data = JSON.stringify(data);
+    localStorage.setItem(data);
 }
 
-const  addToMultiDb=(id,value)=>{
+const addToMultiDb = (id, value) => {
 
 }
 
 const getChartData = (key) => {
-    let shoppingCart = {};
+    let data = {};
 
     //get the shopping cart from local storage
     const storedCart = localStorage.getItem(key);
     if (storedCart) {
-        shoppingCart = JSON.parse(storedCart);
+        data = JSON.parse(storedCart);
     }
-    return shoppingCart;
+    return data;
 }
 
 export {
     chartSingleDb,
     chartMultiDb,
-    getChartData
+    getChartData,
+    addToSingleDb
 
 }
